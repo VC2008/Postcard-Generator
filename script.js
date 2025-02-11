200e28aca15d795d060ecdf3b8253fb9267a7a8f
VC2008 <526Vcolon@frhsd.com>
2025-02-11 09:17:09 -0500

    Very tired coding, may need some time at home

---
+++ script.js
@@ -0,0 +1,8 @@
+var states = document.getElementById=('states');
+states.onchange = function() {
+console.log('state index: ' + states.selectedIndex);
+console.log('state name: ' + states.options[states.selectedIndex].text);
+console.log('img/' + states.value + '@2x.jpg');
+document.getElementById('postcard').style.bacq
+}
+
